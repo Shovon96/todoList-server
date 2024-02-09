4694f13f1a0b5fc537ae177d6293878e8107b94c
Shovon96 <fakhruddinshovon@gmail.com>
2024-02-10 01:38:04 +0600

    task post and get api ok

--- index.js
+++ index.js
@@ -4,14 +4,54 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    // credentials: true,
+  })
+);
 app.use(express.json());
 
 // const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.5q3e1dy.mongodb.net/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 async function run() {
   try {
+    const taskCollection = client.db("taskify").collection("alltasks");
+
+    // app.get('/tasks', async (req, res) => {
+    //     const result = await taskCollection.find().toArray();
+    //     res.send(result);
+    //   });
+    
+    //get all the task
+    app.get("/tasks", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await taskCollection
+        .find(query)
+        .sort({ priority: 1 })
+        .toArray();
+      res.send(result);
+    });
+
+    // create task
+    app.post("/task", async (req, res) => {
+      const data = req.body;
+      const result = await taskCollection.insertOne(data);
+      res.send(result);
+    });
+
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
